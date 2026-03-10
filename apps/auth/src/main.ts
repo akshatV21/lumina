f410754b91e3b9acb5ca85ab0cd640863f0bd6f5
Akshat Vishwakarma <a.vishwakarma2110@gmail.com>
2026-03-10 12:43:27 +0530

    Auth setup + Prisma initiated

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import * as cors from 'cors';
+import * as morgan from 'morgan';
 import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const config = app.get<ConfigService>(ConfigService);
+
+  const PORT = config.getOrThrow('PORT');
+
+  app.use(cors());
+  app.use(helmet());
+  app.use(morgan('dev'));
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT, () => {
+    console.log(`Listening to requests on port: ${PORT}`);
+  });
 }
 bootstrap();
