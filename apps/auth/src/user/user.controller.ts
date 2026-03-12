b9dc102aa23d796e4d0e6ccef0e5e86e368629f2
Akshat Vishwakarma <a.vishwakarma2110@gmail.com>
2026-03-12 12:57:27 +0530

    Added bio update endpoint

--- apps/auth/src/user/user.controller.ts
+++ apps/auth/src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Patch } from '@nestjs/common'
 import { UserService } from './user.service'
+import { Auth, AuthUser, HttpResponse, User } from '@app/utils'
+import { UserBioDto } from './dtos/bio.dto'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Patch('bio')
+  @Auth()
+  async httpUpdateBio(@Body() data: UserBioDto, @AuthUser() user: User): HttpResponse {
+    await this.userService.bio(data, user)
+    return { success: true, message: 'User bio updated successfully.' }
+  }
 }
--- apps/auth/src/user/user.service.ts
+++ apps/auth/src/user/user.service.ts
@@ -1,7 +1,16 @@
 import { DatabaseService } from '@app/database'
 import { Injectable } from '@nestjs/common'
+import { UserBioDto } from './dtos/bio.dto'
+import { User } from '@app/utils'
 
 @Injectable()
 export class UserService {
   constructor(private readonly db: DatabaseService) {}
+
+  async bio(data: UserBioDto, user: User) {
+    await this.db.user.update({
+      where: { id: user.id },
+      data: { bio: data.bio },
+    })
+  }
 }
